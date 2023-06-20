192309e426eb0a792e6116810370add6db994b13
lee <tlkj1@naver.com>
2023-06-21 08:03:39 +0900

    김영한 스프링 기본 할인정책Test

---
+++ src/main/java/hello/core/order/OderApp.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.Member;
+public class OderApp {
+    public static void main(String[] args) {
+        MemberService memberService=new MemberServiceImpl();
+        OrderService orderService=new OrderServiceImpl();
+
+        Long memberId=1L;
+        Member member= new Member(memberId,"memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "ItemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order = " + order.calculatePrice());
+    }
+}
