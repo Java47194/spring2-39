34ebd2edb3fa865db9ae4b20ec10c6a671bc4ea0
lee <tlkj1@naver.com>
2023-06-21 09:58:16 +0900

    김영한 스프링 기본 AppConfig 관심사 분리(DIP,OCP)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
@@ -10,8 +11,16 @@ import hello.core.member.Member;
 public class OrderServiceImpl implements OrderService{
 
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; //dip: 추상에만 의존 해라
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 고정 할인 금액 dip위반
+    //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); dip위반
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member=memberRepository.findById(memberId);
