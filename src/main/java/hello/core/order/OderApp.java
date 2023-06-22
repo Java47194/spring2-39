7e37b3f5d6bec998d5d6a312bca462e4930e39fb
lee <tlkj1@naver.com>
2023-06-22 09:33:24 +0900

    자바 -> 스프링 전환

--- src/main/java/hello/core/order/OderApp.java
+++ src/main/java/hello/core/order/OderApp.java
@@ -5,11 +5,19 @@ import hello.core.member.Grade;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.Member;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class OderApp {
     public static void main(String[] args) {
-        AppConfig appConfig=new AppConfig();
+      /*  AppConfig appConfig=new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService();*/
+
+        ApplicationContext app= new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = app.getBean("mmm", MemberService.class);
+        OrderService orderService = app.getBean("orderService", OrderService.class);
 
 
         Long memberId=1L;
