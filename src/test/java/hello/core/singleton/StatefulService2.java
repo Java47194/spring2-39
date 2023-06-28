18eadac75fcfd0b0b5850e8d86018d4dbd73b7ae
lee <tlkj1@naver.com>
2023-06-28 12:26:46 +0900

    싱글톤 방식의 주의점

---
+++ src/test/java/hello/core/singleton/StatefulService2.java
@@ -0,0 +1,15 @@
+package hello.core.singleton;
+
+
+//무상태(stateless) 개발
+public class StatefulService2 {
+
+
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+        return price;
+    }
+
+
+}
---
+++ src/test/java/hello/core/singleton/StatefulService2Test.java
@@ -0,0 +1,43 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class StatefulService2Test {
+
+    @Test
+    void statefulServiceSingleton2() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(testConfig2.class);
+        StatefulService2 statefulService1 = ac.getBean(StatefulService2.class);
+        StatefulService2 statefulService2 = ac.getBean(StatefulService2.class);
+
+
+        int userA = statefulService1.order("userA", 10000);
+
+        int userB = statefulService2.order("userB" , 20000);
+
+        System.out.println(userA);
+        System.out.println(userB);
+
+
+        assertThat(userA).isEqualTo(10000);
+
+
+    }
+
+    static class testConfig2{
+        @Bean
+        public StatefulService2 statefulService2(){
+            return new StatefulService2();
+        }
+
+
+    }
+
+
+}
