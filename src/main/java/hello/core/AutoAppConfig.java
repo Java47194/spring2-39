0228106ddb894a43b9aa1a76ed47a1b715b59f03
lee <tlkj1@naver.com>
2023-06-30 13:25:58 +0900

    탐색위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
-
 // 자동으로 스프링빈을 긁어 모음
 public class AutoAppConfig {
 }
