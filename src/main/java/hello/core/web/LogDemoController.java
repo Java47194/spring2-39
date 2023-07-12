a3ca0e1addbdc0fa5e29f76e4ada03ffa9c7e85e
lee <tlkj1@naver.com>
2023-07-13 08:51:41 +0900

    스코프와 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -1,12 +1,9 @@
 package hello.core.web;
-
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -14,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
 
     @RequestMapping("log-demo")
@@ -22,9 +19,9 @@ public class LogDemoController {
     public String logDemo(HttpServletRequest request) throws InterruptedException {
 
         String requestURL=request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger.getClass() = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
-
+    
         myLogger.log("controller test");
         Thread.sleep(100);
         logDemoService.logic("testId");
